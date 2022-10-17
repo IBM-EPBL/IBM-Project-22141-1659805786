3adc6e26bdc667336b6776fff2b023bec6af22da
srihari-02 <113891643+srihari-02@users.noreply.github.com>
2022-10-17 15:08:39 +0530

    Update assignment 3_traffic light.py

--- Assignments/Team member3 - Srihari/assignment 3_traffic light.py
+++ Assignments/Team member3 - Srihari/assignment 3_traffic light.py
@@ -4,21 +4,21 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-M1=[17,18,27]
-GPIO.setup(M1, GPIO.OUT)
+H1=[20,26,21]
+GPIO.setup(H1, GPIO.OUT)
 
-M=[]
-VAL=[]
+H=[]
+NUM=[]
 td=1
-def light(M,VAL):
-    GPIO.output(M[0], VAL[0])
-    GPIO.output(M[1], VAL[1])
-    GPIO.output(M[2], VAL[2])
+def light(H,NUM):
+    GPIO.output(H[0], NUM[0])
+    GPIO.output(H[1], NUM[1])
+    GPIO.output(H[2], NUM[2])
     time.sleep(td)
  
 while True:
-   light(M1, [1,0,0])
-   light(M1, [0,1,0])
-   light(M1, [0,0,1])
+   light(H1, [1,0,0])
+   light(H1, [0,1,0])
+   light(H1, [0,0,1])
    time.sleep(td)
 
